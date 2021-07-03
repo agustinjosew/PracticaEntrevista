45902a66072293a60efe78f757bf17eee013374c
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-07-03 08:54:06 -0300

    Capas Clientes-Modelos iniciales, Add capa Servicios

---
+++ SVS-Ventas/src/Modelos/Factura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public int Id_Cliente { get; set; }
+        public Cliente Cliente { get; set; }
+        public decimal Iva { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+
+
+    }
+}
---
+++ SVS-Ventas/src/Modelos/FacturaDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos
+{
+    public class FacturaDetalle
+    {
+        public int Id { get; set; }
+        public int Id_Factura { get; set; }
+        public Factura Factura { get; set; }
+
+        public int Id_Producto { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal IVA { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
