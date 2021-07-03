859b74415edc88c933827e5c440913c090a5ff3e
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-07-03 09:51:18 -0300

    TestConexion

--- SVS-Ventas/src/ClienteConsola/Program.cs
+++ SVS-Ventas/src/ClienteConsola/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Servicios;
+using System;
 
 namespace ClienteConsola
 {
@@ -6,7 +7,8 @@ namespace ClienteConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestServicio.TextConexion();
+            Console.Read();
         }
     }
 }
